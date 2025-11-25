2266c19eaec212dcb810ec836d24b028caa96509
Paulo Daniel <92828125+kampello@users.noreply.github.com>
2025-11-25 15:53:55 +0000

    Troquei a pipeline

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Brian DO FF',
-  tagline: 'Dinosaurs are cool',
+  tagline: 'O meu siteé bueda fixolas o Dtchita não 😛😛',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
